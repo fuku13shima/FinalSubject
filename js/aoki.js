8917c57ad5e5d384433de90fd552e156713a2bd1
fukushima <22110051@oic-ok.ac.jp>
2023-06-20 11:21:25 +0900

    ヘッダー等追記

--- js/aoki.js
+++ js/aoki.js
@@ -2,14 +2,19 @@ let butFlg = false;
 
 imageBut.addEventListener('click' , () => {
     let image = document.getElementById('image');
+    let butText = document.getElementById('imageBut');
+    console.log(butText);
+    
     //console.log(image);
     if(butFlg === false){
         butFlg = true;
-        image.innerHTML = "<img src = \"images/aoikiImage\" alt=\"\" width=\"50%\" height=\"50%\">";
+        butText.innerHTML = '画像非表示';
+        image.innerHTML = '<img src = \"images/aoikiImage\" alt=\"\" width=\"40%\" height=\"40%\">';
     
     }else{
         butFlg = false;
-        image.innerHTML = "";
+        butText.innerHTML = '画像表示';
+        image.innerHTML = '';
     }
 
 });
