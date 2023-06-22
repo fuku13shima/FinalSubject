cfc9e84b3a68a5be2d9b68677e9f1353a3ac2683
nanso <22110024@oic-ok.ac.jp>
2023-06-22 12:45:50 +0900

    count botan no tuika

--- js/index.js
+++ js/index.js
@@ -38,7 +38,23 @@
 })();
 
 
+/*カウントボタン 作成者:ナンバ*/
+let counter, btnAdd;
+let n;
 
+function addCount(){
+    n++;
+    counter.innerHTML = n;
+}
+
+window.addEventListener("load", ()=>{
+    // 起動時の処理
+    counter = document.getElementById("counter");
+    btnAdd = document.getElementById("cntBut");
+    n = 0;
+
+    btnAdd.addEventListener("click", addCount);
+});
 
 
 
