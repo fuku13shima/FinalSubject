b55ef8529da79492aa0a557b151028c03b3736de
aoka2 <aoka543222@gmail.com>
2023-06-23 09:51:31 +0900

    kobashi.htmlとkobashi.jsに追記、改変を加えました。
    jsファイルはsjisからutf-8に変更をしました。
    headerの変更部分を適用しました。

---
+++ js/kobashi.js
@@ -0,0 +1,20 @@
+let butFlg = false;
+
+imageBut.addEventListener('click', () => {
+    let image = document.getElementById('image');
+    let butText = document.getElementById('imageBut');
+    console.log(butText);
+
+    //console.log(image);
+    if (butFlg === false) {
+        butFlg = true;
+        butText.innerHTML = '画面非表示';
+        image.innerHTML = '<img src = \"images/kobashi_smill.jpg\" alt=\"\" width=\"40%\" height=\"40%\">';
+
+    } else {
+        butFlg = false;
+        butText.innerHTML = '画面表示';
+        image.innerHTML = '';
+    }
+
+});
