7a9d753898280b3c09905ec3f3936d67973a0e77
fukushima <22110051@oic-ok.ac.jp>
2023-06-23 15:31:17 +0900

    aoki.js、aoki.html画像表示方法変更

--- js/aoki.js
+++ js/aoki.js
@@ -1,20 +1,25 @@
 let butFlg = false;
+//ページ読み込み時の画像の状態
+let nowDisplay = image.style.display;
 
 imageBut.addEventListener('click' , () => {
     let image = document.getElementById('image');
     let butText = document.getElementById('imageBut');
+    
     console.log(butText);
     
     //console.log(image);
     if(butFlg === false){
         butFlg = true;
         butText.innerHTML = '画像非表示';
-        image.innerHTML = '<img src = \"images/aoikiImage\" alt=\"\" width=\"40%\" height=\"40%\">';
+        image.style.display = 'none';
+        // image.innerHTML = '<img src = \"images/aoikiImage\" alt=\"\" width=\"40%\" height=\"40%\">';
     
     }else{
         butFlg = false;
         butText.innerHTML = '画像表示';
-        image.innerHTML = '';
+        image.style.display = nowDisplay;
+        // image.innerHTML = '';
     }
 
 });
