68b4a869e28a95275be5704331fa6d7b828c9b40
aoka2 <aoka543222@gmail.com>
2023-06-24 03:00:05 +0900

    error repairing

--- js/kobashi.js
+++ js/kobashi.js
@@ -9,7 +9,7 @@ imageBut.addEventListener('click', () => {
     if (butFlg === false) {
         butFlg = true;
         butText.innerHTML = '画面非表示';
-        image.innerHTML = '<img src = \"images/kobashi_smill.jpg\" alt=\"\" width=\"40%\" height=\"40%\">';
+        image.innerHTML = '<img src = "images/kobashi_smill.jpg" alt="" width="40%" height="40%">';
 
     } else {
         butFlg = false;
