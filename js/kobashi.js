26ca6b0ddcbd6291c523bafabc34db7badf352cd
aoka2 <aoka543222@gmail.com>
2023-06-24 15:43:45 +0900

    kobashi.jsに画像表示アニメーションを追記しました。

--- js/kobashi.js
+++ js/kobashi.js
@@ -9,17 +9,26 @@ imageBut.addEventListener('click', () => {
     if (butFlg === false) {
         butFlg = true;
         butText.innerHTML = '画面非表示';
-        image.innerHTML = '<img src = "images/kobashi_smill.jpg" alt="" width="40%" height="40%">';
+        image.innerHTML = '<img src = "images/kobashi_smill.jpg" alt="" width="40%" height="40%" opacity="0%">';
+        $('#image').css({ opacity: '0' });
+        setTimeout(function () {
+            $("#image").stop().animate({ opacity: '1' }, 120);
+
+        },10);
 
     } else {
         butFlg = false;
         butText.innerHTML = '画面表示';
-        image.innerHTML = '';
+        setTimeout(function () {
+            $("#image").stop().animate({ opacity: '0' }, 120);
+
+        }, 10);
+        setTimeout(function () {
+            image.innerHTML = '';
+        },130);
     }
 
 });
-
-
 
 
 
